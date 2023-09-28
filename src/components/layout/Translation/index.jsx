ea5b99041ca2e7ab261140e78b049c93c7f5b58d
nairut <turian@hotmail.com>
2023-09-28 09:51:23 -0300

    Update index.jsx

--- src/components/layout/Translation/index.jsx
+++ src/components/layout/Translation/index.jsx
@@ -10,8 +10,8 @@ const Translation = () => {
   const handleClick = () => {
     window.scrollTo({
       top: 0,
-      behavior: 'smooth', 
-    });
+      behavior: 'smooth', \zxc\xzcx
+    }); 
   }
   return (
     <div className='section' style={{marginTop: MarginTop}}>
